6fd5bab8c9f1b914a15f457052ce6d5df483f003
olowoyinka <olowofelayinka@gmail.com>
2022-10-09 22:17:16 +0100

    Refactor the Code

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,14 +1,12 @@
-from abc import ABC
-
-from car import Car
+from battery.battery import Battery
 
 
-class SpindlerBattery(Car, ABC):
+class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        super().__init__(last_service_date)
+        self.last_service_date = last_service_date
         self.current_date = current_date
 
-    def battery_should_be_serviced(self):
+    def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
         if service_threshold_date < datetime.today().date():
             return True
