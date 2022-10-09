3a658b208b046369e7df7a674d37bde28e4c6e1b
olowoyinka <olowofelayinka@gmail.com>
2022-10-09 21:24:48 +0100

    Completed the task

---
+++ engine/battery/nubbin_battery.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+from car import Car
+
+
+class NubbinBattery(Car, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+
+    def battery_should_be_serviced(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date():
+            return True
+        else:
+            return False
--- engine/model/rorschach.py
+++ engine/model/rorschach.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 from engine.willoughby_engine import WilloughbyEngine
+from engine.battery.nubbin_battery import NubbinBattery
 
 
-class Rorschach(WilloughbyEngine):
+class Rorschach(WilloughbyEngine, NubbinBattery):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+        if self.battery_should_be_serviced() or self.engine_should_be_serviced():
             return True
         else:
             return False
